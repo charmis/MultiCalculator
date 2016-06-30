f03b9b07b6d540764f8d50c257f37984cab93117
Charmis Varghese <charmispv@users.noreply.github.com>
2016-06-30 11:45:41 -0400

    Added font awesome and applied styles to components

--- app/components/footer/footerModule.js
+++ app/components/footer/footerModule.js
@@ -1,6 +1,6 @@
 angular.module("footerModule", [])
 .component("footer", {
-  template: "<h1>Footer</h1>",
+  templateUrl: '/app/components/footer/footerview.html',
   controller: function(){
     
   }
