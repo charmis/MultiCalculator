a9a39a1c426b0f3e653f01ef38a32dc529e2fbce
Charmis Varghese <charmispv@users.noreply.github.com>
2016-07-01 11:20:19 -0400

    Aligning buttons completed.

--- app/components/standardcalculator/standardcalculator.spec.js
+++ app/components/standardcalculator/standardcalculator.spec.js
@@ -1,12 +1,26 @@
 describe('calculator', function () {
 
   beforeEach(module('standardcalculatorModule'));
+	var element;
+  var scope;
 
-  var $controller;
-
-  beforeEach(inject(function(_$controller_){
-    $controller = _$controller_;
+  beforeEach(inject(function ($rootScope, $compile) {
+    scope = $rootScope.$new();
+    element = angular.element('<standardcalculator></standardcalculator>');
+    element = $compile(element)(scope);
+    scope.outside = '1.5';
+    scope.$apply();
   }));
+
+	var controller;
+	beforeEach(function () {
+		controller = element.controller('myComponent');
+	});
+
+	it('should expose my title', function () {
+		expect(controller.myTitle).toBeDefined();
+		expect(controller.myTitle).toBe('Unit Testing AngularJS');
+	});
 
   describe('sum', function () {
 		it('1 + 1 should equal 2', function () {
@@ -16,7 +30,7 @@ describe('calculator', function () {
 			$scope.y = 2;
 			$scope.sum();
 			expect($scope.z).toBe(3);
-		});	
+		});
 	});
 
 });
