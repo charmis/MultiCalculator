63a76a5bfb68519db4c666899c2ef939b8621d35
Charmis Varghese <charmis@users.noreply.github.com>
2016-07-08 14:38:28 -0400

    Gulp tasks sequenced.

--- src/gulpfile.js
+++ src/gulpfile.js
@@ -7,14 +7,16 @@ var uglify = require('gulp-uglify');
 var gulpIf = require('gulp-if');
 var inject = require('gulp-inject');
 
-gulp.task('default', ['useref'], function () {
-    gulp.src('dist/js/main.min.js')
-        .pipe(angularFilesort())
-        .pipe(gulpDebug())
-        .pipe(gulpPrint());
+gulp.task('copy_components', function () {
+    gulp.src('app/**/*.html').pipe(gulp.dest('dist/app'));
 });
 
-gulp.task('useref', function () {
+gulp.task('copy_bower_components', ['copy_components'], function () {
+    gulp.src(['bower_components/**/*.min.js', 'bower_components/**/*.min.css'])
+        .pipe(gulp.dest('dist/bower_components'));
+});
+
+gulp.task('useref', ['copy_bower_components'], function () {
     return gulp.src('index.html')
         .pipe(gulpDebug())
         .pipe(useref())
@@ -22,13 +24,7 @@ gulp.task('useref', function () {
         .pipe(gulp.dest('dist'))
 });
 
-gulp.task('copy_bower_components', function () {
-    gulp.src(['bower_components/**/*.min.js', 'bower_components/**/*.min.css'])
-        .pipe(gulpDebug())
-        .pipe(gulp.dest('dist/bower_components'));
-});
-
-gulp.task('inject_bower_components', function () {
+gulp.task('inject_bower_components', ['useref'], function () {
     var target = gulp.src('dist/index.html');
     // It's not necessary to read the files (will speed up things), we're only after their paths: 
     var sources = gulp.src(['dist/bower_components/**/*.js', 'dist/bower_components/**/*.css'], { read: false });
@@ -37,7 +33,10 @@ gulp.task('inject_bower_components', function () {
         .pipe(gulp.dest('dist'));
 });
 
-
-gulp.task('copy_components', function () {
-    gulp.src('app/**/*.html').pipe(gulp.dest('dist/app'));
+gulp.task('default', ['inject_bower_components'], function () {
+    console.log('default task');
+    //gulp.src('dist/js/main.min.js')
+    //  .pipe(angularFilesort())
+    //.pipe(gulpDebug())
+    //.pipe(gulpPrint());
 });
